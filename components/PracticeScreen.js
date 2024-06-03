b3117890fb8ac5ac8b406b9564db71d61c73d54f
foreaser <79438829+foreaser@users.noreply.github.com>
2024-06-03 23:01:40 +0900

    Update PracticeScreen.js

--- components/PracticeScreen.js
+++ components/PracticeScreen.js
@@ -1,47 +1,124 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import {
+  Text,
   View,
   TextInput,
   Button,
-  Text,
-  StyleSheet,
+  Image,
   Alert,
   ScrollView,
-  Keyboard,
+  StyleSheet,
 } from 'react-native';
 
 export default function PracticeScreen({ navigate }) {
   const [word, setWord] = useState('');
   const [words, setWords] = useState([]);
+  const [timeLeft, setTimeLeft] = useState(10);
+  const [gameOver, setGameOver] = useState(false);
+  const [wrongWord, setWrongWord] = useState(null);
+  const [timerStarted, setTimerStarted] = useState(false);
   const scrollViewRef = useRef();
+  const timerRef = useRef(null);
+
+  useEffect(() => {
+    if (timerStarted && timeLeft > 0 && !gameOver) {
+      timerRef.current = setTimeout(() => {
+        setTimeLeft(timeLeft - 1);
+      }, 1000);
+    } else if (timeLeft === 0 && !gameOver) {
+      setGameOver(true);
+      Alert.alert('패배! 시간을 초과했습니다.');
+      return;
+    }
+
+    return () => clearTimeout(timerRef.current);
+  }, [timeLeft, gameOver, timerStarted]);
+
+  useEffect(() => {
+    if (gameOver) {
+      setTimerStarted(false);
+    }
+  }, [gameOver]);
 
   const handleInputChange = (input) => {
     setWord(input);
+    setWrongWord(null);
   };
 
   const handleInputSubmit = () => {
+    if (gameOver) {
+      Alert.alert('게임이 종료되었습니다. 다시 시작하세요!');
+      return;
+    }
+
     if (words.includes(word)) {
-      Alert.alert('이전에 입력된 단어입니다. 다른 단어를 입력하세요!');
+      setWrongWord(word);
+      Alert.alert(
+        `${word}는(은) 이전에 입력된 단어입니다. 다른 단어를 입력하세요!`
+      );
       return;
     }
 
     if (words.length > 0) {
       const lastWord = words[words.length - 1];
       if (lastWord[lastWord.length - 1] !== word[0]) {
-        Alert.alert('다른 단어를 입력하세요!');
+        setWrongWord(word);
+        Alert.alert(`${word}는(은) 틀린 단어입니다. 다른 단어를 입력하세요!`);
         return;
       }
+    } else {
+      setTimerStarted(true);
     }
 
     setWords([...words, word]);
     setWord('');
-
+    setTimeLeft(10);
     scrollViewRef.current.scrollToEnd({ animated: true });
   };
 
+  const handleRestart = () => {
+    setWords([]);
+    setWord('');
+    setTimeLeft(10);
+    setGameOver(false);
+    setTimerStarted(false);
+  };
+
+  const getBackgroundColor = () => {
+    if (gameOver) {
+      return 'white';
+    }
+    if (timeLeft <= 0) {
+      return 'rgba(255, 0, 0, 0.8)';
+    } else if (timeLeft <= 1) {
+      return 'rgba(255, 0, 0, 0.5)';
+    } else if (timeLeft <= 2) {
+      return 'rgba(255, 0, 0, 0.3)';
+    }
+    return 'white';
+  };
+
+  const getTimerImage = () => {
+    const images = {
+      10: require('./assets/timer10.png'),
+      9: require('./assets/timer9.png'),
+      8: require('./assets/timer8.png'),
+      7: require('./assets/timer7.png'),
+      6: require('./assets/timer6.png'),
+      5: require('./assets/timer5.png'),
+      4: require('./assets/timer4.png'),
+      3: require('./assets/timer3.png'),
+      2: require('./assets/timer2.png'),
+      1: require('./assets/timer1.png'),
+      0: require('./assets/timer0.png'),
+    };
+    return images[timeLeft];
+  };
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { backgroundColor: getBackgroundColor() }]}>
       <View style={styles.topEmptySpace}></View>
+      <Image source={getTimerImage()} style={styles.timerImage} />
       <View style={styles.wordsWrapper}>
         <ScrollView
           style={styles.wordsContainer}
@@ -57,15 +134,22 @@ export default function PracticeScreen({ navigate }) {
       </View>
       <View style={styles.inputWrapper}>
         <TextInput
-          style={styles.input}
+          style={[styles.input, { color: wrongWord ? 'red' : 'black' }]}
           value={word}
           onChangeText={handleInputChange}
           placeholder="단어 입력"
           onSubmitEditing={handleInputSubmit}
           blurOnSubmit={false}
+          editable={!gameOver}
         />
         <Button title="입력" onPress={handleInputSubmit} />
       </View>
+      {gameOver && (
+        <View style={styles.gameOverMessage}>
+          <Text style={styles.gameOverText}>패배! 시간을 초과했습니다.</Text>
+          <Button title="다시 시작" onPress={handleRestart} />
+        </View>
+      )}
       <View style={styles.footer}>
         <Button title="뒤로가기" onPress={() => navigate('Home')} />
       </View>
@@ -82,6 +166,12 @@ const styles = StyleSheet.create({
   },
   topEmptySpace: {
     flex: 1,
+  },
+  timerImage: {
+    width: 87,
+    height: 100,
+
+    marginBottom: 20,
   },
   wordsWrapper: {
     flex: 2,
@@ -115,9 +205,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 10,
     marginBottom: 10,
   },
+  gameOverMessage: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 20,
+  },
+  gameOverText: {
+    fontSize: 20,
+    color: 'red',
+    marginBottom: 10,
+  },
   footer: {
     flex: 3,
-    justifyContent: 'top',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     width: '100%',
   },
