87acac327257cc9276990f37dd8d93506c2db868
foreaser <79438829+foreaser@users.noreply.github.com>
2024-06-03 23:01:09 +0900

    Update App.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import HomeScreen from './components/HomeScreen';
 import PracticeScreen from './components/PracticeScreen';
+import MutualGame from './components/MutualGame';
 
 export default function App() {
   const [currentScreen, setCurrentScreen] = useState('Home');
@@ -10,8 +11,10 @@ export default function App() {
     <View style={styles.container}>
       {currentScreen === 'Home' ? (
         <HomeScreen navigate={setCurrentScreen} />
-      ) : (
+      ) : currentScreen === 'Practice' ? (
         <PracticeScreen navigate={setCurrentScreen} />
+      ) : (
+        <MutualGame navigate={setCurrentScreen} />
       )}
     </View>
   );
